61bd802ec814cef66857755e81184ae0d72d7915
Stefan-smiles <s.cookesec@gmail.com>
2024-10-18 14:53:16 +0100

    friyay

--- script.js
+++ script.js
@@ -24,6 +24,51 @@ let superOven = Number(localStorage.getItem("superOven")) || 0;
 let kitchen = Number(localStorage.getItem("kitchen")) || 0;
 
 //functions shorcuts
+//API
+async function fetchupgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  const data = await response.json();
+}
+
+const grannyValue = 1;
+const superGrannyValue = 5;
+const ovenValue = 1;
+const superOvenValue = 5;
+const kitchenValue = 250;
+const grannyCost = 10;
+const ovenCost = 10;
+const superGrannyCost = 10;
+const superOvenCost = 10;
+const kitchenCostGranny = 10;
+const kitchenCostOven = 10;
+
+grabGranny.title =
+  "this will cost " +
+  grannyCost +
+  " cookies, and will give you " +
+  grannyValue +
+  " cookies p/s";
+grabOven.title =
+  "this will cost " +
+  ovenCost +
+  " cookies, and will give you " +
+  ovenValue +
+  " cookies p/s";
+grabSuperGranny.title =
+  "this will cost " +
+  superGrannyCost +
+  " grannies, and will give you " +
+  superGrannyValue +
+  " cookies p/s";
+grabSuperOven.title =
+  "this will cost " +
+  superOvenCost +
+  " ovens, and will give you " +
+  superOvenValue +
+  " cookies p/s";
+
 function dave() {
   //display
   cookieDisplay.textContent = cookies;
@@ -56,58 +101,62 @@ cookieBtn.addEventListener("click", function () {
 });
 
 //upgrades
-/* const upgradeslvl1 = {
-  grannies[cost:10,increase: 1],oven["cost":15, increase:2]
-}
-/const upgradeslvl2 = {
-  superGrannies[cost:25,increase 15] superOven[cost:25, increase:30]
-}*/
-//Grannies
+
 grabGranny.addEventListener("click", function () {
-  if (cookies >= 10) {
-    cps = cps + 1;
-    cookies = cookies - 10;
+  if (cookies >= grannyCost) {
+    cps = cps + grannyValue;
+    cookies = cookies - grannyCost;
     grannies = grannies + 1;
     dave();
+  } else {
+    alert("you don't have enough cookies for that");
   }
 });
 grabSuperGranny.addEventListener("click", function () {
-  if (grannies >= 25) {
-    cps = cps - 25 + 15;
-    grannies = grannies - 25;
+  if (grannies >= superGrannyCost) {
+    cps = cps + superGrannyValue - grannyValue * 10;
+    grannies = grannies - superGrannyCost;
     superGrannies = superGrannies + 1;
     dave();
+  } else {
+    alert("you don't have enough grannies for that");
   }
 });
 grabOven.addEventListener("click", function () {
-  if (cookies >= 10) {
-    cps = cps + 1;
-    cookies = cookies - 10;
+  if (cookies >= ovenCost) {
+    cps = cps + ovenValue;
+    cookies = cookies - ovenCost;
     oven = oven + 1;
     dave();
+  } else {
+    alert("you don't have enough cookies for that");
   }
-  /*else {
-    alert 
-    }*/
 });
 grabSuperOven.addEventListener("click", function () {
-  if (oven >= 25) {
-    cps = cps - 25 + 15;
-    oven = oven - 25;
+  if (oven >= superOvenCost) {
+    cps = cps - ovenValue * 10 + superOvenValue;
+    oven = oven - superOvenCost;
     superOven = superOven + 1;
     dave();
+  } else {
+    alert("you don't have enough ovens for that");
   }
 });
 kitchens.addEventListener("click", function () {
-  if ((superOven >= 10, superGrannies >= 10)) {
-    cps = cps - 300 + 500;
-    superOven = superOven - 10;
-    superGrannies = superGrannies - 10;
+  if (superOven >= kitchenCostOven && superGrannies >= kitchenCostGranny) {
+    cps = cps - superOvenValue * 10 - superGrannyValue * 10 + kitchenValue;
+    superOven = superOven - kitchenCostOven;
+    superGrannies = superGrannies - kitchenCostGranny;
     kitchen = kitchen + 1;
     dave();
+  } else if (superGrannies < 10 && superOven < 10) {
+    alert("you don't have enough Super Grannies or Super Ovens for that");
+  } else if (superGrannies < 10) {
+    alert("you don't have enough Super Grannies for that");
+  } else if (superOven < 10) {
+    alert("you don't have enough Super Ovens for that");
   }
 });
-//
 
 //Die button
 die.addEventListener("click", function () {
